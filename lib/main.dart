34458f3436d2460d32751c15581cf7bc88e26317
1priyanshi <priyanshigupta2973@gmail.com>
2020-05-05 23:28:28 +0530

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+import 'CustomLoginForm.dart';
+void main() => runApp(Login());
+
+class Login extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    final appTitle = "Login";
+    return MaterialApp(
+      title: appTitle,
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text(appTitle),
+        ),
+        backgroundColor: Colors.blueGrey,
+        body: CustomLoginForm(),
+      ),
+    );
+  }
+}
